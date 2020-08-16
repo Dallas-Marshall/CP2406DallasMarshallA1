849e4a42215dd65d3d052cd970461c7661c5e0f3
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2020-08-16 15:21:03 +1000

    Add file read and store

--- src/RainfallAnalyser.java
+++ src/RainfallAnalyser.java
@@ -1,9 +1,31 @@
 // TODO: you must use TextIO in your solution to read from the data files
 import textio.TextIO;
+import java.util.ArrayList;
 
 public class RainfallAnalyser {
     public static void main(String[] args) {
         // TODO: add your solution code here
+        TextIO.readFile("resources/MountSheridanStationCNS.csv");
+
+//        array to store arrays of each line of data split
+        ArrayList<String[]> dataLines = new ArrayList<>();
+
+
+        while (!TextIO.eof()) {
+            String line = TextIO.getln();
+            String[] lineSplit = line.split(",", -1);
+            dataLines.add(lineSplit);
+        }
+
+//        TESTING ONLY - PRINT OUT ARRAY CONTENTS!!!!!!!!!!!
+        for(String[] line : dataLines){
+            System.out.print(" [");
+//          iterate through line array and print elements
+            for (String element : line) {
+                System.out.print(element + ", ");
+            }
+            System.out.println("]");
+        }
     }
 
     // TODO: consider using static methods
