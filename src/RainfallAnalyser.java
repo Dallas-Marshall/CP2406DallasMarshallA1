9fcb966e64cf4b792b5d2b2b64de37d931344932
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2020-08-17 13:37:56 +1000

    Add month grouping and monthlyRainfallSum

--- src/RainfallAnalyser.java
+++ src/RainfallAnalyser.java
@@ -5,13 +5,15 @@ import textio.TextIO;
 public class RainfallAnalyser {
     public static void main(String[] args) {
         // TODO: add your solution code here
-//        System.out.println("please enter the path to the file: ");
-//        String filename = TextIO.getln();
-        String filename = "resources/MountSheridanStationCNS.csv";
+        System.out.println("please enter the path to the file: ");
+        String filename = TextIO.getln();
+//        String filename = "resources/MountSheridanStationCNS.csv";
         TextIO.readFile(filename);
         TextIO.getln(); // remove header record
 
-        int lineCount = 0;
+//        create record file to save in
+
+        double monthlyRainfallSum = 0;
         while (!TextIO.eof()) {
             String line = TextIO.getln();
             String[] lineSplit = line.split(",", -1);
@@ -41,13 +43,21 @@ public class RainfallAnalyser {
             int day = Integer.parseInt(lineSplit[4]);
             double rainfallMeasurement = Double.parseDouble(lineSplit[5]);
 
-//            System.out.println(year);
-//            System.out.println(month);
-//            System.out.println(day);
-//            System.out.println(rainfallMeasurement);
-//            System.out.println();
-
-            lineCount++;
+//            check if in new month
+            if (day != 1) {
+                monthlyRainfallSum += rainfallMeasurement;
+//                monthDayCount++;
+            } else {
+//                calculate min max
+//                create identifying information for record
+//                Save month details to file
+                System.out.printf("%1.2f \n", monthlyRainfallSum);
+                monthlyRainfallSum = rainfallMeasurement;
+//                monthDayCount = 1;
+            }
+            if (TextIO.eof()) { // data ends halfway through month
+                System.out.printf("%1.2f \n", monthlyRainfallSum);
+            }
         }
     }
 
