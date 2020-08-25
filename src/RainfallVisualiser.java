3129e382e7ddae471917c54c1730d2fe0a09a7c3
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2020-08-25 14:31:59 +1000

    Add graphing and axis labels

--- src/RainfallVisualiser.java
+++ src/RainfallVisualiser.java
@@ -8,6 +8,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import textio.TextIO;
+import javafx.scene.control.Label;
 
 /**
  * This file can be used to draw a chart that effectively represents rainfall data.  Just fill in
@@ -21,11 +22,47 @@ public class RainfallVisualiser extends Application {
      */
     public void drawPicture(GraphicsContext g, int width, int height) {
 
-        // TODO: draw the x-axis and y-axis
-        g.strokeLine(125, height - 50, width - 25, height - 50);
-        g.strokeLine(125, height - 50, 125, 25);
+        g.setFill(Color.WHITE);
+        g.fillRect(0, 0, width, height);
 
-        // TODO: draw the monthly totals as a bar chart
+        int INDEX_OF_YEAR = 0;
+        int INDEX_OF_MONTH = 1;
+        int INDEX_OF_RAINFALL_TOTAL = 2;
+        int X_AXIS_HEIGHT = height - 40;
+        int startingValueX = 125;
+
+//        create axis
+        g.strokeLine(startingValueX, X_AXIS_HEIGHT, width - 25, X_AXIS_HEIGHT);
+        g.strokeLine(startingValueX, X_AXIS_HEIGHT, startingValueX, 25);
+        g.strokeText("Year", (width / 2.0), height - 10, 25);
+        g.strokeText("Rainfall (mL)", 10, (height / 2.0), 60);
+        g.strokeText("Monthly Rainfall Totals per Year", (width / 2.0 - 30),  20, 125);
+
+        TextIO.getln(); // remove header record
+
+        int currentValueX = startingValueX; // start graphing inside axis
+        int yearsGraphed = 0;
+        while (!TextIO.eof()) {
+            String line = TextIO.getln();
+            String[] lineSplit = line.split(",", -1);
+
+            int year = Integer.parseInt(lineSplit[INDEX_OF_YEAR]);
+            int month = Integer.parseInt(lineSplit[INDEX_OF_MONTH]);
+            double monthlyRainfallSum = Double.parseDouble(lineSplit[INDEX_OF_RAINFALL_TOTAL]);
+            monthlyRainfallSum = monthlyRainfallSum / 4; // scale down to fit larger totals
+
+//            alternate bar colours per year
+            if (month == 1) {
+                yearsGraphed++;
+                if (yearsGraphed % 2 == 0) {
+                    g.setFill(Color.BLACK);
+                } else {
+                    g.setFill(Color.RED);
+                }
+            }
+            g.fillRect(currentValueX, X_AXIS_HEIGHT - monthlyRainfallSum, 4, monthlyRainfallSum);
+            currentValueX += 5; // move x coordinate across for each new bar
+        }
     } // end drawPicture()
 
 
